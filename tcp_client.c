74f109785db3fe8960a1a09daec97bd546ec17e7
ernestoheres <94608266+ernestoheres@users.noreply.github.com>
2022-11-24 12:47:05 +0100

    Add files via upload
    innit

---
+++ tcp_client.c
@@ -0,0 +1,60 @@
+
+#include  <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main (int argc, char *argv[]) {
+	
+	while(1) {
+	//name from command line argument
+	if(argv[1] == NULL || argv[2] == NULL || argv[3] == NULL) {
+	printf("please enter all arguments \n");
+	return 0;
+	}
+	
+	char client_message[256] = {0} ;
+	strcat(client_message,argv[1]);
+	//CREATE SOCKET
+	int netsocket;
+	netsocket = socket(AF_INET, SOCK_STREAM, 0);
+	
+	// set an address for the socket
+	int port = atoi(argv[3]);
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(port);
+	if(argv[2] == 0) {
+		server_address.sin_addr.s_addr = INADDR_ANY;
+	}
+	else {
+		server_address.sin_addr.s_addr = inet_addr(argv[2]);
+	}
+	int connection = connect(netsocket,(struct sockaddr *) &server_address,
+	sizeof(server_address));
+	if(connection ==  -1) {
+		printf("There was an error making a connection \n\n");
+	}
+
+	
+	//receive data
+	char server_response[256];
+	recv(netsocket, &server_response, sizeof(server_response),0);
+	//send client name to server 
+	send(netsocket, client_message, sizeof(client_message),0);
+	//print data from server respnse
+	printf("The server sent data: %s\n", server_response);
+	
+	//close socket
+	close(netsocket);
+	
+	sleep(60);
+	}	
+	return 0;
+	
+}
+
---
+++ tcp_server.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
+
+
+//removes letters from a string
+void parseInts(FILE* fp,char* string){
+	char *p = string;	
+	while(*p) {
+		//checks if the current character is a digit
+		if(isdigit(*p) || ( (*p=='-' ||*p=='+') && isdigit(*(p+1)) )) {
+			long val = strtol(p, &p, 10);
+			fprintf(fp, "%ld", val);
+		}
+		//else go to next char
+		else {
+			p++;
+		}
+	}
+}
+//removes digits from a string
+char* parseChars(char* input) {
+	char* dest = input;
+	char* src = input;
+	
+	while(*src) {
+	 //skips iteration if it detects a digit
+	 if(isdigit(*src)) {src++; continue;}
+	 *dest++ = *src++;	
+	}
+	*dest = '\0';
+	return input;
+}
+int main() {
+	 char server_message[256]  = "reached the server";
+	 //get text file and open it
+	 char *filename = "log.txt";
+         //create socket
+	 int netsocket;
+	 netsocket = socket(AF_INET, SOCK_STREAM, 0);
+	 
+	//define addr struct
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9002);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	
+	//bind
+	bind(netsocket, (struct sockaddr*) &server_address,
+        sizeof(server_address));
+	//listen
+	listen(netsocket, 5);
+	while(1){
+	int client_socket;
+	client_socket = accept(netsocket,NULL,NULL);
+	//checks if the text file opened correctly
+	FILE *fp = fopen(filename, "a+");
+	if(fp == NULL) {
+	 printf("Error opening the text file: %s",filename);
+	 return -1;
+	}
+	//receive data/name from client
+	char client_message[256], *p = client_message, *p2 = client_message, *dest = client_message;
+	send(client_socket, server_message, sizeof(server_message),0);
+	recv(client_socket, &client_message, sizeof(client_message),0);
+	  close(client_socket);
+	//write data received from client to text file
+	//checks if message  is the authorized length
+	if(strlen(client_message) == 17) {
+		printf("The client sent data %s\n", client_message);
+		//prints client message to log.txt
+		fprintf(fp, "%s\n" , client_message);
+		fprintf(fp, " ints:\n");
+		parseInts(fp,client_message);
+		fprintf(fp,"\n chars:\n");
+		fprintf(fp,"%s\n",parseChars(client_message));
+		}
+	else {
+		printf("unauthorized message\n");
+	}
+	//closing text file and socket
+	fclose(fp);	
+	}
+         return 0;
+	
+}
